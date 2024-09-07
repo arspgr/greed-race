3100a1ba94273359b3aeeb9163ffcb10d10dec55
arspgr <arspgr>
2024-09-07 23:32:04 +0300

    display game style

--- src/components/DisplayGame/DisplayGame.tsx
+++ src/components/DisplayGame/DisplayGame.tsx
@@ -1,5 +1,9 @@
 import { FC, useContext } from "react";
 import greedRaceImage from '@/images/greed-race.png';
+import crown from '@/images/crown.png';
+import moneyBag from '@/images/money-bag.png';
+import stackOfCoins from '@/images/stack-of-coins.png';
+import buyTicketBtn from '@/images/buy-ticket-btn.png';
 import './DisplayGame.css';
 import { useGameService } from "@/services/game-service";
 import { Button, Spinner } from "@telegram-apps/telegram-ui";
@@ -20,12 +24,18 @@ export const DisplayGame: FC = () => {
                 <Spinner size="l"></Spinner>
             ) : !!activeGame ? (
                 <>
-                    <div className="text-headline">NEXT</div>
-                    <img src={greedRaceImage}></img>
-                    <div className="text-usual">starts in: {<DisplayTimer expiryTimestamp={activeGame.endsAt}></DisplayTimer>}</div>
-                    <div className="text-usual">Jackpot {activeGame.prize.jackpot.value.toString()} x {activeGame.prize.jackpot.cnt.toString()}</div>
-                    <div className="text-usual">Major prizes {activeGame.prize.major.value.toString()} x {activeGame.prize.major.cnt.toString()}</div>
-                    <div className="text-usual">Minor prizes {activeGame.prize.minor.value.toString()} x {activeGame.prize.minor.cnt.toString()}</div>
+                    <div className="text-headline welcome-to">WELCOME TO</div>
+                    <img className="logo-img" src={greedRaceImage}></img>
+                    <div className="next-draw">
+                        <div className="text-usual blue-shadow">NEXT DRAW</div>
+                        <div className="text-usual blue-shadow">comes in: {<DisplayTimer expiryTimestamp={activeGame.endsAt}></DisplayTimer>}</div>
+                    </div>
+                    <br></br>
+                    <div className="text-usual blue-shadow prize-pool">PRIZE POOL:</div>
+                    <div className="text-usual">{activeGame.prize.jackpot.value.toString()}x <img className="prize-icon" src={crown}></img> JACKPOT {activeGame.prize.jackpot.cnt.toString()}</div>
+                    <div className="text-usual">{activeGame.prize.major.value.toString()}x <img className="prize-icon" src={moneyBag}></img> MAJOR PRIZES {activeGame.prize.major.cnt.toString()}</div>
+                    <div className="text-usual">{activeGame.prize.minor.value.toString()}x <img className="prize-icon" src={stackOfCoins}></img> MINOR PRIZES {activeGame.prize.minor.cnt.toString()}</div>
+                    <div className="text-usual blue-shadow total-racers">TOTAL RACERS</div>
                     <Button onClick={() => buyTicket(tonConnectUI, activeGame.asset, api, activeGame._id)}>BUY TICKET FOR {activeGame.ticketPrice}</Button>
                 </>
             ) : null
